c5417d817f685bdbaf74e238772a032edf05edcc
yuenwinyun <114036811@qq.com>
2021-03-27 21:17:43 +0800

    fix: fix type

--- LinkedList/LC61.rotateList.test.ts
+++ LinkedList/LC61.rotateList.test.ts
@@ -1,6 +1,12 @@
 import { deserializeListNode, serializeListNode } from "./ListNode";
 import { rotateList } from "./LC61.rotateList";
 
+interface TestProps {
+    head: number[];
+    k: number;
+    result: number[];
+}
+
 test.each`
     head               | k             | result
     ${[1, 2, 3, 4, 5]} | ${2}          | ${[4, 5, 1, 2, 3]}
@@ -8,7 +14,7 @@ test.each`
     ${[1, 2, 3]}       | ${2000000000} | ${[2, 3, 1]}
     ${[]}              | ${0}          | ${[]}
     ${[]}              | ${1}          | ${[]}
-`("", ({ head, k, result }) => {
+`("", ({ head, k, result }: TestProps) => {
     const listNodes = deserializeListNode(head);
     const actualResult = serializeListNode(rotateList(listNodes, k));
     expect(actualResult).toEqual(result);
