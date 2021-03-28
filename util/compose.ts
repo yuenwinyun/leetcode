a2fb86aa2a787b93f4ad67f0933cf50e7fb15f67
yuenwinyun <114036811@qq.com>
2021-03-28 09:44:37 +0800

    fix: fix compose

--- util/compose.ts
+++ util/compose.ts
@@ -1,8 +1,20 @@
-export default function compose<R>(fn: () => R): () => R;
-export default function compose<R, V>(fn: (v: V) => R): (v: V) => R;
-export default function compose<R1, R2>(fn2: (r: R1) => R2, fn1: () => R1): () => R2;
-export default function compose<R1, R2, R3>(fn3: (r: R2) => R3, fn2: (r: R1) => R2, fn1: () => R1): () => R3;
-export default function compose<R1, R2, R3, V1>(fn3: (r: R2) => R3, fn2: (r: R1) => R2, fn1: (v: V1) => R1): () => R3;
-export default function compose(...fns: Function[]) {
-    return (x: any) => fns.reduceRight((v, f) => f(v), x);
+export default function compose<T1, T2, T3>(fn3: (r: T2) => T3, fn2: (r: T1) => T2, fn1: () => T1): () => T3;
+export default function compose<V1, T1, T2, T3>(
+    fn3: (r: T2) => T3,
+    fn2: (r: T1) => T2,
+    fn1: (v1: V1) => T1
+): (v1: V1) => T3;
+export default function compose<V1, V2, T1, T2, T3>(
+    fn3: (r: T2) => T3,
+    fn2: (r: T1) => T2,
+    fn1: (v1: V1, v2: V2) => T1
+): (v1: V1, v2: V2) => T3;
+export default function compose<T>(...fns: Function[]) {
+    return (v: T) => {
+        let res = v;
+        for (const fn of fns.reverse()) {
+            res = fn(res);
+        }
+        return res;
+    };
 }
