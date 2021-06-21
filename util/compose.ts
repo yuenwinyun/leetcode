c0485b0f8a203e54c58a3c86afd662ba9da1ea7b
yuen <yuencheung0@gmail.com>
2021-06-21 18:30:04 +0800

    refactor: refactort

--- util/compose.ts
+++ util/compose.ts
@@ -1,22 +1,14 @@
-type LastOfArray<T extends readonly any[]> = T extends [...infer N, infer P] ? P : never;
+type LastOfArray<T extends readonly any[]> = T extends [...infer _, infer Last] ? Last : never;
+
+type InferParamAndReturnTypeInArray<T> = {
+    [K in keyof T]: T[K] extends (...values: infer V) => infer R ? (...values: V) => R : T[K];
+};
 
 export function compose<
     T extends readonly ((...values: any[]) => any)[],
-    Result extends LastOfArray<
-        {
-            [K in keyof T]: T[K] extends (...values: infer V) => infer R ? (...values: [...V]) => R : T[K];
-        }
-    >,
+    R extends LastOfArray<InferParamAndReturnTypeInArray<T>>,
 >(...fns: [...T]) {
-    return (
-        fns.length === 0
-            ? <T>(v: T) => v
-            : fns.length === 1
-            ? fns[0]
-            : fns.reduce(
-                  (a, b) =>
-                      (...args: any[]) =>
-                          a(b(...args)),
-              )
-    ) as Result;
+    return ((...args: any[]) => {}) as R extends never ? <T>(v: T) => T : R;
 }
+
+const emptyArgsCompose = compose(); // FIXME: type compose returnType as <T>(v: T) => v if no args provided
