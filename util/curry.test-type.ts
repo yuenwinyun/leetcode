dacd03bb1e4ad36f343cad4570451452b431e148
yuenwinyun <114036811@qq.com>
2021-07-08 17:06:48 +0800

    refactor: refactor

--- util/curry.test-type.ts
+++ util/curry.test-type.ts
@@ -3,8 +3,15 @@ import { Expect, Equal } from "@type-challenges/utils";
 
 const curried1 = curry((a: number, b: number, c: string) => a === b + parseInt(c), 1);
 const curried2 = curry((a: number, b: string) => a === parseInt(b));
+const curried3 = curry((a: { name: string }, b: { age: number }, c: string) => ({ a, b, c }), { name: "a" });
 
 type TestCases = [
     Expect<Equal<typeof curried1, (p1: number) => (p2: string) => boolean>>,
     Expect<Equal<typeof curried2, (p1: number) => (p2: string) => boolean>>,
+    Expect<
+        Equal<
+            typeof curried3,
+            (p1: { age: number }) => (p2: string) => { a: { name: string }; b: { age: number }; c: string }
+        >
+    >,
 ];
--- util/curry.test.ts
+++ util/curry.test.ts
@@ -5,4 +5,8 @@ export function testCurry() {
     const test = curry((a: number, b: number) => a === b);
     assert.equal(test(1)(1), true);
     assert.equal(test(1)(2), false);
+
+    const test2 = curry((a: number, b: number) => a + b, 1);
+    assert.equal(test2(1), 2);
+    assert.equal(test2(2), 3);
 }
