576ca8376c881c62316798c6adca63170bac7085
yuenwinyun <114036811@qq.com>
2021-08-19 06:41:03 +0800

    chore: format codes

--- dp/LC300.longestIncreasingSubsequence.ts
+++ dp/LC300.longestIncreasingSubsequence.ts
@@ -1,13 +1,13 @@
 export function longestIncreasingSubsequence(nums: number[]): number {
-    const dp: number[] = Array.from(nums, () => 1)
+    const dp: number[] = Array.from(nums, () => 1);
 
     for (let i = 0; i < nums.length; i++) {
         for (let j = 0; j < nums.length; j++) {
             if (nums[i] > nums[j]) {
-                dp[i] = Math.max(dp[i], 1 + dp[j])
+                dp[i] = Math.max(dp[i], 1 + dp[j]);
             }
         }
     }
 
-    return Math.max(0, ...dp)
+    return Math.max(0, ...dp);
 }
