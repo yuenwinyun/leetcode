d3f689260129f5ea3e152e2c86125ac9904d6237
yuenwinyun <geek@yuenwinyun.com>
2022-02-17 23:42:49 +0800

    fix: fix lc300 implmentation

--- dp/LC300.longestIncreasingSubsequence.test.ts
+++ dp/LC300.longestIncreasingSubsequence.test.ts
@@ -1,7 +1,8 @@
-import { longestIncreasingSubsequence } from "./LC300.longestIncreasingSubsequence";
+import { dp } from './LC300.longestIncreasingSubsequence'
 
-test("longest increasing subsequence", () => {
-    expect(longestIncreasingSubsequence([10, 9, 2, 5, 3, 7, 101, 18])).toBe(4);
-    expect(longestIncreasingSubsequence([0, 1, 0, 3, 2, 3])).toBe(4);
-    expect(longestIncreasingSubsequence([7, 7, 7, 7, 7, 7, 7])).toBe(1);
+test("[LIS] DP", () => {
+    expect(dp([10, 9, 2, 5, 3, 7, 101, 18])).toBe(4);
+    expect(dp([0, 1, 0, 3, 2, 3])).toBe(4);
+    expect(dp([7, 7, 7, 7, 7, 7, 7])).toBe(1);
+    expect(dp([3, 2, 1])).toBe(1);
 });
--- dp/LC300.longestIncreasingSubsequence.ts
+++ dp/LC300.longestIncreasingSubsequence.ts
@@ -1,7 +1,7 @@
-export function longestIncreasingSubsequence(ns: number[]) {
+export function dp(ns: number[]): number {
     const dp = Array.from(ns, () => 1);
     for (let i = 0; i < ns.length; i++) {
-        for (let j = 0; j < ns.length; j++) {
+        for (let j = 0; j < i; j++) {
             if (ns[i] > ns[j]) {
                 dp[i] = Math.max(dp[i], 1 + dp[j]);
             }
